b548318cb8ab5bbf401819bb2d8d02f4106e5bc7
liama <liama@ebay.com>
2018-05-21 23:43:59 +0800

    njjjhjhjkhkh

---
+++ convertor/view.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from .serializers import UserSerializer, GroupSerializer
+
+
+def hello(request):
+    return HttpResponse("Hello world ! ")
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
